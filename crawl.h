62f4cfbbf099f34bc65fe035bdcfe1de68d8d7fa
chenyushuo <297086016@qq.com>
2018-03-18 10:43:02 +0800

    ver 2.01

--- crawl.h
+++ crawl.h
@@ -6,7 +6,7 @@
 using namespace std;
 
 #ifdef CRAWL_CPP
-const string seed = string("http://grs.ruc.edu.cn/third/Professor_emeritus.html");//string("http://www.ruc.edu.cn");
+const string seed = string("http://www.ruc.edu.cn");
 #endif
 
 void crawl();
--- filter.h
+++ filter.h
@@ -9,9 +9,9 @@ using namespace std;
 const string key_word = string("ruc");
 const char *forbidden_string[] = {"@", "#", "(", ")", "[", "]", "{", "}",
 					".txt", ".doc", ".ppt", ".xls", ".pdf", ".PDF", "ftp://", "file://", "telnet://",
-					".jpg", ".png", ".gif", ".bmp", ".rar", ".zip", ".7z", ".mp3", ".mp4", ".MP4",
+					".jpg", ".jpeg", ".png", ".gif", ".bmp", ".rar", ".zip", ".7z", ".mp3", ".mp4", ".MP4",
 					"javascript:void(0)", "javascript:;",
-					".flv", ".wmv", ".avi", ".rmvb", ".mpeg",
+					".flv", ".wmv", ".avi", ".rmvb", ".mpeg", ".qt", ".mpg", ".mkv", ".rm",
 					"twitter", "google", "facebook", "END"};
 const char space_char[] = "\t" "\f" "\v" "\n" "\r" " ";
 #endif
